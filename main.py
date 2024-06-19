bdb3a77b99c6cfe08405eb08080da0e9464c4c08
Slavik <yaroslav.salko71@gmail.com>
2024-06-19 16:29:23 +0200

    user login form

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template, request, redirect
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bcrypt import bcrypt
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-class Item(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), unique = True, nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(100), unique = True, nullable = False)
+
+    def __repr__(self):
+        return f'{self.username}'
+
+
+class Services(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
     price = db.Column(db.Integer, nullable = False)
@@ -17,13 +44,37 @@ class Item(db.Model):
         return f'{self.title}'
 
 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        mail = User.query.filter_by(email = email.data).first()
+        if mail:
+            raise ValidationError('That username is taken. Please choose a different one.')
+        
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
 with app.app_context():
     db.create_all()
 
 
 @app.route('/')
 def index():
-    items = Item.query.order_by(Item.price).all()
+    items = Services.query.order_by(Services.price).all()
     return render_template('index.html', data=items)
 
 
@@ -31,10 +82,25 @@ def index():
 def contact():
     return render_template('contact.html')
 
+@app.route('/registre', methods = ['GET', "POST"])
+def registre_user():
+    if request.method == 'POST':
+        ...
 
-@app.route('/support')
-def support():
-    return render_template('support.html')
+
+
+@app.route('/login', methods = ['GET', "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 @app.route('/create', methods = ['POST', 'GET'])
@@ -43,7 +109,7 @@ def create():
         title = request.form['title']
         price = request.form['price']
         
-        item = Item(title=title, price=price )
+        item = Services(title=title, price=price )
         try:
             db.session.add(item)
             db.session.commit()
